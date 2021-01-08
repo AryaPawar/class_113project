579d62b9d3f9089ca78888fbc87a5e6af8401bbe
AryaPawar <72245486+AryaPawar@users.noreply.github.com>
2021-01-08 10:23:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(640, 480);
+    canvas.position(110, 250);
+
+}
+function draw(){
+
+fill(0, 128, 0);
+stroke(0, 128, 0);
+circle(50, 470, 70);
+circle(450, 450, 70);
+}
